8fd4247c903a9d16524e664d8a7a46a08594ab7c
my name <myemailid@domain.com>
2022-04-15 19:03:46 -0400

    added spiner

---
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkout = () => {
+    return (
+        <div>
+            <h2>Please check out ypur booking</h2>
+        </div>
+    );
+};
+
+export default Checkout;
